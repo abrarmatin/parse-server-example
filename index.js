54a0882a6adcc784b7ea4ca79e60fd67c6da6b49
abrarmatin <abrar.matin@gmail.com>
2022-01-07 14:20:20 -0500

    upaded place holder values to be more relevant

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ if (!databaseUri) {
 const config = {
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || 'azulSocial',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse', // Don't forget to change to https if needed
+  serverURL: process.env.SERVER_URL || 'https://arcane-badlands-69566.herokuapp.com', // Don't forget to change to https if needed
   liveQuery: {
     classNames: ['Posts', 'Comments'], // List of classes to support for query subscriptions
   },
